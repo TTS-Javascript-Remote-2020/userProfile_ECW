1c21d51f8d1f315ea6c3bfaa4a84b30355eed12b
Elliott Woodward <elliottcwoodward@gmail.com>
2020-05-18 13:40:26 -0400

    Do homework, install node

---
+++ app.js
@@ -0,0 +1,30 @@
+const userProfile = {
+  name: 'Elliott Woodward',
+  address: '42 LTU&A Street',
+  city: 'Asheville',
+  state: 'North Carolina',
+  zip: '28806',
+  avatar: 'me.jpg'
+}
+
+function getProfileUpdate() {
+  const name = 'e-dawg';
+  const address = '21 Jump Street';
+  const city = 'Woodfin';
+  const zip = '28804';
+  const avatar = 'cool_pic.jpg';
+  return {
+    name: name,
+    address: address,
+    city: city,
+    zip: zip,
+    avatar: avatar
+  };
+}
+
+function updateProfile(obj) {
+  Object.assign(userProfile, obj);
+}
+
+updateProfile(getProfileUpdate());
+console.log(userProfile);
